670f3c303b006ff5b184dc2e3c1559ec46123e9e
AlexMaurin1981 <155809065+AlexMaurin1981@users.noreply.github.com>
2024-03-04 23:35:32 +0300

    business logic for Hibernate

--- JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,48 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
+
+   public static UserDao userDao = new UserDaoHibernateImpl();
+
     public void createUsersTable() {
+
+        userDao.createUsersTable();
+        System.out.println("Таблица создана");
 
     }
 
     public void dropUsersTable() {
 
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+        userDao.saveUser(name, lastName, age);
+        System.out.println("имя пользователя - " + name + " добавлено в базу");
     }
 
     public void removeUserById(long id) {
 
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        System.out.println(userDao.getAllUsers().toString());
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
 
+        userDao.cleanUsersTable();
+        System.out.println("Таблица очищена");
     }
 }
